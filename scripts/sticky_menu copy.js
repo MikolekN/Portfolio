6e9947c98bf5e8ab315714069e5d23aeef2a344a
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-07-17 21:54:43 +0200

    Delete sticky_menu copy.js

--- scripts/sticky_menu copy.js
+++
@@ -1,9 +0,0 @@
-let menu = document.getElementById("menu");
-let offset = menu.offsetTop;
-window.onscroll = function () {
-  if (window.scrollY >= offset) {
-    menu.classList.add("sticky");
-  } else {
-    menu.classList.remove("sticky");
-  }
-};
