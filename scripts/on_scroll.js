4621726e3c91ac832e9ad4e1e1711fb435cffece
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-07-17 21:58:14 +0200

    Update on_scroll.js

--- scripts/on_scroll.js
+++ scripts/on_scroll.js
@@ -1,3 +1,3 @@
-import { stickyMenu } from "./scripts/sticky_menu.js";
+import { stickyMenu } from "./sticky_menu.js";
 import { scrollTopButton } from "./scroll_top_button.js";
 window.onscroll = stickyMenu();
