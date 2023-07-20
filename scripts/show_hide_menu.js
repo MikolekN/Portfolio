9985d0d0a863ccf76fd6182bd9818c22986917a9
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-07-20 21:20:54 +0200

    Finished menu

--- scripts/show_hide_menu.js
+++ scripts/show_hide_menu.js
@@ -1,7 +1,10 @@
 var menu = document.getElementById("menu")
+var icons = document.getElementsByClassName("fa")
 function showMenu() {
     menu.style.right = "0"
 }
 function hideMenu() {
-    menu.style.right = "-200px"
+    menu.style.right = "-100px"
 }
+window.showMenu = showMenu
+window.hideMenu = hideMenu
