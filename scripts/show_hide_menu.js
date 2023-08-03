78b91e0acef998edfd86c911ffb68f8ce0ae2f2d
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-08-03 17:51:49 +0200

    Header rework, added main menu

--- scripts/show_hide_menu.js
+++ scripts/show_hide_menu.js
@@ -1,10 +1,9 @@
-var menu = document.getElementById("menu")
-var icons = document.getElementsByClassName("fa")
+var menu = document.getElementById("header-menu")
 function showMenu() {
     menu.style.right = "0"
 }
 function hideMenu() {
-    menu.style.right = "-150px"
+    menu.style.right = "-100px"
 }
 window.showMenu = showMenu
 window.hideMenu = hideMenu
