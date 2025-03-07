98297ed94cb9ad328fc458bf0ddb074d9b80f67e
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-03-07 20:21:13 +0100

    Contact details fix

--- scripts/sticky_menu.js
+++ scripts/sticky_menu.js
@@ -1,5 +1,5 @@
 let menu = document.getElementById("main-menu-wrapper")
-let menu_placholder = document.getElementById("main-menu-placeholder")
+let menu_placeholder = document.getElementById("main-menu-placeholder")
 let e = document.querySelectorAll("section")
 let offset = menu.offsetTop
 let height = menu.offsetHeight
@@ -7,10 +7,10 @@ function stickyMenu() {
   if (window.scrollY >= offset) {
     menu.style.position = "fixed"
     menu.style.top = '0'
-    menu_placholder.style.height = menu.offsetHeight + 'px'
+    menu_placeholder.style.height = menu.offsetHeight + 'px'
   } else {
     menu.style.position = 'static'
-    menu_placholder.style.height = menu.offsetHeight + 'px'
+    menu_placeholder.style.height = menu.offsetHeight + 'px'
   }
 };
 window.addEventListener('scroll', stickyMenu)
